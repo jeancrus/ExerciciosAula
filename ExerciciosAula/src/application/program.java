f33d4904e2151d6f79d7c98d597e51c46b1fd171
Jean Crus <jean.carlos@pucpr.edu.br>
2019-01-17 11:03:49 -0200

    Exercicio aula 178 generics

--- ExerciciosAula/src/application/program.java
+++ ExerciciosAula/src/application/program.java
@@ -1,10 +1,29 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import services.PrintService;
+
 public class program {
 
 	public static void main(String[] args) {
-		int x = 7 | 8;
-		System.out.println(x);
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		PrintService<Integer> ps = new PrintService<Integer>();
+		
+		System.out.print("How many values? ");
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			int value = sc.nextInt();
+			ps.addValue(value);
+		}
+		
+		ps.print();
+		System.out.println("First: " + ps.first());
+		
+		sc.close();
 	}
 
 }
