8653044cc0c532ecea01c9af54c8653d643d16b8
Jean Crus <jean.carlos@pucpr.edu.br>
2019-01-18 15:33:09 -0200

    Set Hash,TreeSet,LinkedHash and predicate in removeIf

--- ExerciciosAula/src/application/program.java
+++ ExerciciosAula/src/application/program.java
@@ -1,25 +1,45 @@
 package application;
 
-import entities.Client;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class program {
 
 	public static void main(String[] args) {
-		Client c1 = new Client("Maria","maria@gmail.com");
-		Client c2 = new Client("Maria","maria@gmail.com");
+		/*
+		Set<String> set = new LinkedHashSet<>();
 		
-		String s1 = "Test";
-		String s2 = "Test";
+		set.add("TV");
+		set.add("Tablet");
+		set.add("Notebook");
 		
-		String s3 = new String("Test");
-		String s4 = new String("Test");
 		
-		System.out.println(c1.hashCode());
-		System.out.println(c2.hashCode());
-		System.out.println(c1.equals(c2));
-		System.out.println(c1 == c2);
-		System.out.println(s1 == s2);
-		System.out.println(s3 == s4);
-		System.out.println(s1 == s3);
+		System.out.println(set.contains("Notebook"));
+		
+		set.removeIf(x -> x.charAt(0) == 'T'); //remover x tal que primeira letra seja igual T
+		//set.removeIf(x -> x.length() >= 3); //remover x tal que tamanho de x é maior ou igual a 3
+		
+		for (String p: set) {
+			System.out.println(p);
+		}*/
+		
+		Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
+		Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
+		
+		//union
+		Set<Integer> c = new TreeSet<>(a);
+		c.addAll(b);
+		System.out.println(c);
+		
+		//intersection
+		Set<Integer> d = new TreeSet<>(a);
+		d.retainAll(b);
+		System.out.println(d);
+		
+		//diference
+		Set<Integer> e = new TreeSet<>(a);
+		e.removeAll(b);
+		System.out.println(e);
 	}
 }
