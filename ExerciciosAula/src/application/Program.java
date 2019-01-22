6856a81e3013ec34632a1f60ca6b29bde3c980d8
Jean Crus <jean.carlos@pucpr.edu.br>
2019-01-22 16:29:43 -0200

    Aula 188 demo map set

--- ExerciciosAula/src/application/Program.java
+++ ExerciciosAula/src/application/Program.java
@@ -1,40 +1,30 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.time.Instant;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
-
-import entities.LogEntry;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter file full path: ");
-		String path = sc.nextLine();
+		Map<String, String> cookies = new TreeMap<>();
 		
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+		cookies.put("username", "Maria");
+		cookies.put("email", "maria@gmail.com");
+		cookies.put("phone", "99711122");
+		
+		cookies.remove("email");
+		cookies.put("phone", "232324242");
+		
+		System.out.println("Contains 'phone' key: " + cookies.containsKey("phone"));
+		System.out.println("Phone number: " + cookies.get("phone"));
+		System.out.println("Email: " + cookies.get("email"));
+		System.out.println("Size: " + cookies.size());
+		
+		System.out.println("All cookies: ");
+		for (String key : cookies.keySet()) {
+			System.out.println(key + ": " + cookies.get(key));
 			
-			Set<LogEntry> set = new HashSet<>();
-			
-			String line = br.readLine();
-			while (line != null) {
-				String[] fields = line.split(" ");
-				String username = fields[0];
-				Date moment = Date.from(Instant.parse(fields[1]));
-				set.add(new LogEntry(username, moment));
-				line = br.readLine();
-			}
-			System.out.println("Total user: " + set.size());
-		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
 		}
-		sc.close();
 	}
 
 }
