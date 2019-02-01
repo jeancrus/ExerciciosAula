c9d5b31c9a59e17796bd52645b8b836073be1752
Jean Crus <jean.carlos@pucpr.edu.br>
2019-02-01 17:11:44 -0200

    Aula 195 - predicate class

--- ExerciciosAula/src/application/Program.java
+++ ExerciciosAula/src/application/Program.java
@@ -4,20 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import entities.Product;
+import util.ProductPredicate;
 
 public class Program {
 	
-	public static int compareProducts(Product p1, Product p2) {
-		return p1.getPrice().compareTo(p2.getPrice());
-	}
 
 	public static void main(String[] args) {
 		List<Product> list = new ArrayList<>();
-		list.add(new Product("TV", 900.00));
-		list.add(new Product("Notebook", 1200.00));
-		list.add(new Product("Tablet", 450.00));
-		list.sort(Program::compareProducts);
-		list.forEach(System.out::println);
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		int b = 100;
+		list.removeIf(new ProductPredicate());
+		for (Product p : list) {
+			System.out.println(p);
+			
+		}
 	}
 
 }
