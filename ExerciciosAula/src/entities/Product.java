fdbac21e0f7ff3bd1f5bca04082092f63cc28089
Jean Crus <jean.carlos@pucpr.edu.br>
2019-02-04 12:54:09 -0200

    Aula 196 - Consumer com reference com metodo não estático

--- ExerciciosAula/src/entities/Product.java
+++ ExerciciosAula/src/entities/Product.java
@@ -24,6 +24,10 @@ public class Product{
 		p.setPrice(p.getPrice() * 1.1);
 	}
 	
+	public void nonStaticPriceUpdate() {
+		this.price = this.price * 1.1;
+	}
+	
 	@Override
 	public String toString() {
 		return name + ", " + String.format("%.2f", price);
