6efcd40ea0610442780863f0498b7c866c18196c
Jean Crus <jean.carlos@pucpr.edu.br>
2019-02-04 12:21:54 -0200

    Aula 196 - Consumer implementação da interface

--- ExerciciosAula/src/application/Program.java
+++ ExerciciosAula/src/application/Program.java
@@ -2,24 +2,25 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.function.Predicate;
 
 import entities.Product;
+import util.PriceUpdate;
 
 public class Program {
 	
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
 		List<Product> list = new ArrayList<>();
 		list.add(new Product("Tv", 900.00));
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
-		list.removeIf(p -> p.getPrice() >= 100);
-		for (Product p : list) {
-			System.out.println(p);
-			
-		}
+		
+		list.forEach(new PriceUpdate());
+		list.forEach(System.out::println);
 	}
 
 }
