c6d988abb3255b0fc75e33088a5b66d88bb035f8
Jean Crus <jean.carlos@pucpr.edu.br>
2019-02-06 11:27:40 -0200

    Aula 198 - Função ineficiente

--- ExerciciosAula/src/application/Program.java
+++ ExerciciosAula/src/application/Program.java
@@ -3,12 +3,9 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import entities.Product;
-import util.UpperCaseName;
+import model.services.ProductService;
 
 public class Program {
 	
@@ -21,8 +18,13 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
-		names.forEach(System.out::println);
+		ProductService ps = new ProductService();
+		
+		double sum = ps.filteredSum(list);
+		
+		System.out.println("Sum = " + String.format("%.2f", sum));
+		
+		
 	}
 
 }
