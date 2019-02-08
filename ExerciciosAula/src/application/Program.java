56aa09781d9a1594fe844b1d4323651bf1f4dcc0
Jean Crus <jean.carlos@pucpr.edu.br>
2019-02-08 16:55:30 -0200

    Aula 199 - Demo streams

--- ExerciciosAula/src/application/Program.java
+++ ExerciciosAula/src/application/Program.java
@@ -1,29 +1,27 @@
 package application;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
-
-import entities.Product;
-import model.services.ProductService;
+import java.util.stream.Stream;
 
 public class Program {
 	
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
-		List<Product> list = new ArrayList<>();
-		list.add(new Product("Tv", 900.00));
-		list.add(new Product("Mouse", 50.00));
-		list.add(new Product("Tablet", 350.50));
-		list.add(new Product("HD Case", 80.90));
+		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
+		Stream<Integer> st1 = list.stream().map(x -> x * 10);
+		System.out.println(Arrays.toString(st1.toArray()));
 		
-		ProductService ps = new ProductService();
-		double sum = ps.filteredSum(list, p -> p.getPrice() < 100.0);
+		Stream<String> st2 = Stream.of("Maria", "Alex", "Bob");
+		System.out.println(Arrays.toString(st2.toArray()));
 		
-		System.out.println("Sum = " + String.format("%.2f", sum));
+		Stream<Integer> st3 = Stream.iterate(0, x -> x + 2);
+		System.out.println(Arrays.toString(st3.limit(10).toArray()));
 		
-		
+		Stream<Long> st4 = Stream.iterate(new Long[] {0L, 1L}, p -> new Long[] {p[1], p[0] + p[1]}).map(p -> p[0]);
+		System.out.println(Arrays.toString(st4.limit(10).toArray()));
 	}
 
 }
